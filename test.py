92afccc70ab6f6eafad1204d6ec9ad64966d887f
Pascal Thomet <pthomet@gmail.com>
2022-07-09 10:05:53 +0200

    add imgui/implot demos

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from lg_hello_imgui import hello_imgui, imgui, implot
 
 
-COUNT = 0
+class AppState:
+    count = 0
+    show_implot_demo: bool = False
+    show_imgui_demo: bool = False
 
 
-def my_gui():
-    global COUNT
+def my_gui(app_state: AppState):
+
     if imgui.button("Click me"):
-        COUNT += 1
-    imgui.text(f"Hello, you clicked {COUNT} times")
+        app_state.count += 1
+    imgui.text(f"Hello, you clicked {app_state.count} times")
 
+    _, app_state.show_imgui_demo = imgui.checkbox("ImGui demo", app_state.show_imgui_demo)
+    _, app_state.show_implot_demo = imgui.checkbox("Implot demo", app_state.show_implot_demo)
 
-hello_imgui.run(my_gui)
+    if app_state.show_imgui_demo:
+        imgui.show_demo_window()
+    if app_state.show_implot_demo:
+        implot.show_demo_window()
+    
+
+if __name__ == "__main__":
+    app_state = AppState()
+    implot.create_context()
+    hello_imgui.run(lambda: my_gui(app_state))
